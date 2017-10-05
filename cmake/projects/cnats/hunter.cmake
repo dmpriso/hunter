2527549751eda24f54f4d1fd0fb944c560e3f398
dmpriso <modler.daniel@gmail.com>
2017-10-05 11:38:02 +0200

    added hunter version 1.6.0

--- cmake/projects/cnats/hunter.cmake
+++ cmake/projects/cnats/hunter.cmake
@@ -17,6 +17,17 @@ hunter_add_version(
     2ACCBEABE5FD3CC52EB15D6403E0C3EFF06DD849
 )
 
+hunter_add_version(
+    PACKAGE_NAME
+    cnats
+    VERSION
+    "1.6.0"
+    URL
+    "https://github.com/nats-io/cnats/archive/v1.6.0.tar.gz"
+    SHA1
+    E3B459797092BE186E1BD4FFB84F92AB6C482EF4
+)
+
 # Probably more versions for real packages...
 
 # Pick a download scheme
