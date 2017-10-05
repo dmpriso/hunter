66f7dd8d47fa9a5086107f8a9cbbffabd1101bf0
dmpriso <modler.daniel@gmail.com>
2017-10-05 16:19:56 +0200

    use dmpriso1

--- cmake/projects/Libevent/hunter.cmake
+++ cmake/projects/Libevent/hunter.cmake
@@ -8,9 +8,9 @@ include(hunter_pick_scheme)
 
 hunter_add_version(
     PACKAGE_NAME Libevent
-    VERSION "2.1.8"
-    URL "https://github.com/hunter-packages/libevent/archive/v2.1.8-p2.tar.gz"
-    SHA1 "492abb962dad3071f34bbe4975e28768fd29edf0")
+    VERSION "2.1.9"
+    URL "https://github.com/dmpriso/libevent/archive/dmpriso1.tar.gz"
+    SHA1 "F5462FD61FC9F31D3BBBD29392CE137E2839711B")
 hunter_cmake_args(Libevent CMAKE_ARGS
                   EVENT__DISABLE_TESTS=ON
                   EVENT__DISABLE_SAMPLES=ON
